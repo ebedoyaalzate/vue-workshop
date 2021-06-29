f4a9f5554c0f024b2d0cbf8b087ab50f03fd9cdf
ebedoyaalzate <esteban.bedoya.ext@kavak.com>
2021-06-28 22:04:16 -0500

    add other properties to vue instance

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const app = new Vue({
   el: '#app',
-  data: {
-    title: 'Hello World'
+  data: function() {
+    return {
+      title: 'Hello World',
+      firstName: 'Esteban',
+      lastName: 'Bedoya',
+      motherName: null,
+      finalResult: null,
+    }
+  },
+  computed: {
+    fullName () {
+      return `Mi nombre es ${this.firstName} ${this.lastName}`
+    }
+  },
+  watch: {
+    motherName (value, oldValue) {
+      console.log(`old value: ${oldValue}`)
+      console.log(`new value: ${value}`)
+    }
+  },
+  methods: {
+    submit() {
+      this.finalResult = `${this.fullName} y mi madre es ${this.motherName}`
+    }
   }
 })
